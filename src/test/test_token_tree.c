e7ccc8faad1a0989eada725e8621132e254c6f7c
ussuratoncachi <redchindaniil@gmail.com>
2023-08-24 00:55:42 +0600

    Added full coverage for token_tree.c

--- src/test/test_token_tree.c
+++ src/test/test_token_tree.c
@@ -45,6 +45,12 @@ static TtContext default_ctx() {
     };
 }
 
+static TokenTree parse_assert(const char* name) {
+    TokenTreeResult res = token_tree_parse(name, default_ctx());
+    ck_assert(res.is_ok);
+    return res.ok;
+}
+
 #define EPS 0.000001
 
 START_TEST(test_token_tree_1) {
@@ -104,12 +110,78 @@ START_TEST(test_token_tree_5) {
 }
 END_TEST
 
+START_TEST(test_token_tree_clone) {
+    TokenTree a = parse_assert("(x, y, (z, (w, a)))");
+    TokenTree b = token_tree_clone(&a);
+    ck_assert_int_eq(token_tree_depth(&a), token_tree_depth(&b));
+    ck_assert_int_eq(token_tree_max_len(&a), token_tree_max_len(&b));
+    token_tree_free(a);
+    token_tree_free(b);
+}
+END_TEST
+
 
 START_TEST(test_token_tree_err_1) {
     const char* text = ")";
     TokenTreeResult res = token_tree_parse(text, default_ctx());
     ck_assert(not res.is_ok);
     str_free(res.err.text);
+}
+END_TEST
+
+START_TEST(test_token_tree_err_2) {
+    const char* text = "[}";
+    TokenTreeResult res = token_tree_parse(text, default_ctx());
+    ck_assert(not res.is_ok);
+    str_free(res.err.text);
+}
+END_TEST
+
+
+START_TEST(test_token_tree_err_nulltk) {
+    TokenTree tree = parse_assert("(1, 2)");
+    ck_assert(token_tree_get_only_token(&tree) is null);
+    token_tree_free(tree);
+}
+END_TEST
+
+
+START_TEST(test_token_tree_vectors) {
+    TokenTree tree = parse_assert("(([1, [[((2, 3, 4))]]] [5, 6]))");
+    ck_assert_int_eq(token_tree_max_len(&tree), 5);
+    token_tree_free(tree);
+}
+END_TEST
+
+
+START_TEST(test_token_tree_print) {
+
+    StringStream stringstream = string_stream_create();
+    OutStream stream = string_stream_stream(&stringstream);
+
+    TokenTree tree = parse_assert("(([1, [[((2, test_fn 3, 4))]]] [5, 6]))");
+    token_tree_print(&tree, stream);
+    token_tree_free(tree);
+
+    str_t string = string_stream_to_str_t(stringstream);
+    // ck_assert_str_eq(string.string, "(my_variable<=>x,42.000000)");
+    str_free(string);  
+}
+END_TEST
+
+START_TEST(test_token_tree_leading_commas) {
+    TokenTree tree = parse_assert(",,,1,,test_fn 1,2,,1,,1,,,");
+    token_tree_free(tree);
+}
+END_TEST
+
+START_TEST(test_token_tree_unwrap_wrappers) {
+    TokenTree tree = parse_assert("[[[{[({1})]}]]]");
+    tree = token_tree_unwrap_wrappers(tree);
+    ck_assert_int_eq(token_tree_ttype_skip(&tree), TOKEN_NUMBER);
+    ck_assert_int_eq(token_tree_max_len(&tree), 1);
+    ck_assert_int_eq(token_tree_depth(&tree), 1);
+    token_tree_free(tree);
 }
 END_TEST
 
@@ -120,7 +192,14 @@ Suite *token_tree_suite(void) {
   tcase_add_test(tc_core, test_token_tree_3);
   tcase_add_test(tc_core, test_token_tree_4);
   tcase_add_test(tc_core, test_token_tree_5);
+  tcase_add_test(tc_core, test_token_tree_clone);
   tcase_add_test(tc_core, test_token_tree_err_1);
+  tcase_add_test(tc_core, test_token_tree_err_2);
+  tcase_add_test(tc_core, test_token_tree_err_nulltk);
+  tcase_add_test(tc_core, test_token_tree_vectors);
+  tcase_add_test(tc_core, test_token_tree_print);
+  tcase_add_test(tc_core, test_token_tree_leading_commas);
+  tcase_add_test(tc_core, test_token_tree_unwrap_wrappers);
 
   Suite *s = suite_create("Token tree suite");
   suite_add_tcase(s, tc_core);
